4d0f9dcb63780b6cd8c54529198f8b17f4f74e15
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-02-23 12:23:37 +0000

    Changes to server

--- lab1/src/Server.java
+++ lab1/src/Server.java
@@ -1,5 +1,20 @@
 public class Server {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        if(args.length != 1){
+            System.out.println("ERROR: not enough arguments");
+            System.out.print(getUsage());
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
     }
+
+    private static String getUsage(){
+        return
+            "Usage:\n"+
+            "    java Server PORT\n"+
+            "    PORT   Port number that the server shall use to provide the service\n"
+        ;
+    }
+
 }
