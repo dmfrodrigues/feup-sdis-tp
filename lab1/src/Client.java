40e41d821cddbaf98c0e1434568ad3ecbb0f6e3e
BrenoAccioly <breno.accioly91@gmail.com>
2021-02-23 14:58:38 +0000

    Added some error handling

--- lab1/src/Client.java
+++ lab1/src/Client.java
@@ -3,19 +3,21 @@ import java.net.*;
 
 public class Client {
 
+    private static final int TIMEOUT = 3000;
     private static final int MAX_MSG_LEN = 1024;
 
     public static void main(String[] args) throws IOException {
         if (args.length < 4) {
-            System.out.println("Usage: java Client <host> <port> <oper> <opnd>*");
+            printUsage();
             System.exit(1);
         }
 
         InetAddress host = InetAddress.getByName(args[0]);
         int port = Integer.parseInt(args[1]);
         DatagramSocket socket = new DatagramSocket();
+        socket.setSoTimeout(TIMEOUT);
 
-        if(args[2].equals("register"))
+        if(args[2].equals("register") && args.length == 5)
         {
             RegisterMessage msg = new RegisterMessage(args[3], args[4]);
             sendRequest(socket, host, port, msg);
@@ -26,6 +28,7 @@ public class Client {
         }
         else{
             System.out.println("Invalid operation");
+            printUsage();
             System.exit(1);
         }
 
@@ -42,11 +45,23 @@ public class Client {
     private static String getResponse(DatagramSocket socket) throws IOException {
         byte[] buffer = new byte[MAX_MSG_LEN];
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
+        try {
+            socket.receive(packet);
+        } catch (SocketTimeoutException e) {
+            return "ERROR";
+        }
         return new String(packet.getData());
     }
 
     private static void printResult(String[] args, String response){
-        System.out.println("Client: "+ args[2] + " " + args[3] + args[4] + " : "+ response);
+        if(args[2].equals("register"))
+            System.out.println("Client: "+ args[2] + " " + args[3] + args[4] + " : "+ response);
+        else if(args[2].equals("lookup"))
+            System.out.println("Client: "+ args[2] + " " + args[3] + " : "+ response);
+    }
+
+    private static void printUsage(){
+        System.out.println("Usage: java Client <host> <port> register <DNS name> <IP address>");
+        System.out.println("       java Client <host> <port> lookup <DNS name>");
     }
 }
