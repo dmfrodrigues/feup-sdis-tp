43724f130fb472df2f2657b780ac44958db16930
BrenoAccioly <breno.accioly91@gmail.com>
2021-02-23 12:16:28 +0000

    Started client

--- lab1/src/Client.java
+++ lab1/src/Client.java
@@ -1,2 +1,21 @@
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class Client {
+    public static void main(String[] args) throws SocketException {
+        if () {
+            System.out.println("Usage: ...");
+        }
+
+        DatagramSocket socket = new DatagramSocket();
+    }
+
+    private static void sendRequest(){
+
+    }
+
+    private static void receiveReply(){
+
+    }
+
 }
