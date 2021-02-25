70cbd101e9b9c450898da1ec0784c75ea88f880d
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-02-25 14:42:52 +0000

    Changed tests

--- lab2/test/test1.sh
+++ lab2/test/test1.sh
@@ -2,6 +2,9 @@
 set -e
 
 TIMEOUT=10
+SERVICE_PORT=4040
+MCAST_ADDR=230.0.0.1
+MCAST_PORT=4141
 
 test () {
     echo -en "$1\t"
@@ -16,15 +19,15 @@ test () {
 }
 
 cd bin
-timeout $TIMEOUT java Server 4040 230.0.0.1 4141 & PID=$!
+timeout $TIMEOUT java Server $SERVICE_PORT $MCAST_ADDR $MCAST_PORT & PID=$!
 echo "Started server with PID $PID"
 sleep 3
-test "test1-01" "java Client $1 4040 register www.fe.up.pt 192.168.0.1" "Client: register www.fe.up.pt 192.168.0.1 : 1"
-test "test1-02" "java Client $1 4040 register www.fe.up.pt 192.168.0.1" "Client: register www.fe.up.pt 192.168.0.1 : 1"
-test "test1-03" "java Client $1 4040 register www.google.com 123.123.123.123" "Client: register www.google.com 123.123.123.123 : 2"
-test "test1-04" "java Client $1 4040 register www.google.com 123.123.123.123" "Client: register www.google.com 123.123.123.123 : 2"
-test "test1-05" "java Client $1 4040 register web.fe.up.pt 128.128.128.128" "Client: register web.fe.up.pt 128.128.128.128 : 3"
-test "test1-06" "java Client $1 4040 lookup www.fe.up.pt" "Client: lookup www.fe.up.pt : www.fe.up.pt 192.168.0.1"
-test "test1-07" "java Client $1 4040 lookup www.fe.up.pt" "Client: lookup www.fe.up.pt : www.fe.up.pt 192.168.0.1"
-test "test1-08" "java Client $1 4040 lookup www.google.com" "Client: lookup www.google.com : www.google.com 123.123.123.123"
-test "test1-09" "java Client $1 4040 lookup web.fe.up.pt" "Client: lookup web.fe.up.pt : web.fe.up.pt 128.128.128.128"
+test "test1-01" "java Client $MCAST_ADDR $MCAST_PORT register www.fe.up.pt 192.168.0.1" "Client: register www.fe.up.pt 192.168.0.1 : 1"
+test "test1-02" "java Client $MCAST_ADDR $MCAST_PORT register www.fe.up.pt 192.168.0.1" "Client: register www.fe.up.pt 192.168.0.1 : 1"
+test "test1-03" "java Client $MCAST_ADDR $MCAST_PORT register www.google.com 123.123.123.123" "Client: register www.google.com 123.123.123.123 : 2"
+test "test1-04" "java Client $MCAST_ADDR $MCAST_PORT register www.google.com 123.123.123.123" "Client: register www.google.com 123.123.123.123 : 2"
+test "test1-05" "java Client $MCAST_ADDR $MCAST_PORT register web.fe.up.pt 128.128.128.128" "Client: register web.fe.up.pt 128.128.128.128 : 3"
+test "test1-06" "java Client $MCAST_ADDR $MCAST_PORT lookup www.fe.up.pt" "Client: lookup www.fe.up.pt : www.fe.up.pt 192.168.0.1"
+test "test1-07" "java Client $MCAST_ADDR $MCAST_PORT lookup www.fe.up.pt" "Client: lookup www.fe.up.pt : www.fe.up.pt 192.168.0.1"
+test "test1-08" "java Client $MCAST_ADDR $MCAST_PORT lookup www.google.com" "Client: lookup www.google.com : www.google.com 123.123.123.123"
+test "test1-09" "java Client $MCAST_ADDR $MCAST_PORT lookup web.fe.up.pt" "Client: lookup web.fe.up.pt : web.fe.up.pt 128.128.128.128"
