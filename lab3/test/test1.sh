d15dea240f98db509b0ea14b012f4a615ab5831d
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-03-03 14:51:24 +0000

    Fixed lab3 tests

--- lab3/test/test1.sh
+++ lab3/test/test1.sh
@@ -16,6 +16,8 @@ test () {
 }
 
 cd bin
+timeout $TIMEOUT rmiregistry & RMI_PID=$!
+sleep 1
 timeout $TIMEOUT java Server WorkInterface > /dev/null & PID=$!
 echo "Started server with PID $PID"
 sleep 1
@@ -28,3 +30,5 @@ test "test1-06" "java Client localhost WorkInterface lookup www.fe.up.pt" "Clien
 test "test1-07" "java Client localhost WorkInterface lookup www.fe.up.pt" "Client: lookup www.fe.up.pt : www.fe.up.pt 192.168.0.1"
 test "test1-08" "java Client localhost WorkInterface lookup www.google.com" "Client: lookup www.google.com : www.google.com 123.123.123.123"
 test "test1-09" "java Client localhost WorkInterface lookup web.fe.up.pt" "Client: lookup web.fe.up.pt : web.fe.up.pt 128.128.128.128"
+kill $PID
+kill $RMI_PID
